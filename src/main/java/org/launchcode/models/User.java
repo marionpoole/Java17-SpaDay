6b1052f1518b1f8ff4c2e49fb16685fedc23ed9b
Marion Poole <mapoole09@hotmail.com>
2024-12-19 19:35:16 -0600

    Validation Studio Complete

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 6)
     private String password;
 
     public User() {
